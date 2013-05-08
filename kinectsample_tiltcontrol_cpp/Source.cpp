82f72a54cc907c9a443dea4794437e9118eb7c7a
Sen Wang <moli.wang@gmail.com>
2013-05-08 10:50:52 +0800

    偵測 / 設定Kinect Sensor俯仰角

--- kinectsample_tiltcontrol_cpp/Source.cpp
+++ kinectsample_tiltcontrol_cpp/Source.cpp
@@ -5,14 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if( S_OK  == NuiInitialize(NUI_INITIALIZE_FLAG_USES_COLOR))
+	if( S_OK  == NuiInitialize(NUI_INITIALIZE_FLAG_USES_COLOR))
     {
         cout << "成功初始化\n" ;
+
+        long degree = 0 ; 
+        NuiCameraElevationGetAngle(&degree) ;
+
+        cout << "目前角度: " << degree  << endl;
+
+        degree = 20 ;
+        NuiCameraElevationSetAngle(degree) ;
 
      }else
         cout << "初始化失敗\n" ;
 
-	 //控制Kinect的程式碼於此
 
     NuiShutdown() ;
     return 0;
